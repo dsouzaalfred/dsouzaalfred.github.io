176009f4b3f536c0390c9f564c2320b070541b68
Alfred D'Souza <alfred.dsouza@hyd.agency>
2020-08-26 12:08:20 +0100

    initial theme setup

---
+++ _posts/2020-08-26-getting-started-with-react.md
@@ -0,0 +1,28 @@
+---
+layout: post
+title: Getting started with React
+date: 2020-08-26 11:15 +0100
+author: alfred_dsouza
+categories: blog react
+---
+Let's get started with React.
+
+## What is react?
+What is react section will go here
+
+## CRA - Create React App
+What is CRA and why to use it will go here
+
+## Simple App
+A simple react App
+
+```javascript
+const test = () => {
+  return "test";
+}
+```
+
+## Local server
+
+
+## Build
---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Getting started with React",
+        "excerpt":"Let’s get started with React. What is react? What is react section will go here CRA - Create React App What is CRA and why to use it will go here Simple App A simple react App const test = () =&gt; {  return \"test\";}Local server Build ","categories": ["blog","react"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/react/getting-started-with-react/"
+      }]
