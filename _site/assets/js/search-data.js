0a80e01977aec5fdab528ef9dd5c6c108a4f8315
Alfred D'Souza <dsouzaalfred@gmail.com>
2020-09-02 12:42:15 +0100

    add favicons and manifest

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Getting started with React",
         "excerpt":"What is react? React or ReactJS is a component-based JavaScript library for building user interfaces. It is one of the most popular JavaScript UI libraries.Component-based means a complex UI is broken down into small pieces called components. These small components can manage their own state and encapsulate their own logic....","categories": ["blog","react"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/blog/react/getting-started-with-react/"
+        "url": "http://localhost:4000/blog/react/getting-started-with-react/"
       },{
         "title": "JSX - What is that?",
         "excerpt":"In the last post I briefly mentioned JSX, in this post we will have a detailed view of JSX.So what is JXS? JXS is like a template language or a form of markup like HTML that gets complied to JavaScript code by react. React components are all JavaScript, hence we...","categories": ["blog","react"],
         "tags": [],
-        "url": "http://0.0.0.0:4000/blog/react/jsx-what-is-that/"
+        "url": "http://localhost:4000/blog/react/jsx-what-is-that/"
       }]
